e6a91e6cc7c3da16436fcdb6adbf224d2b8ff9bb
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-06 02:26:40 +0800

    test: isolate inlined ownership from value reconstruction

--- tests/fixtures/formal_parameter_fixture.c
+++ tests/fixtures/formal_parameter_fixture.c
@@ -7,10 +7,10 @@
 #define OPTIMIZED_LOCAL_EXPECTED UINT64_C(0x1e3c1e781e3c1ef0)
 #define ARITHMETIC_LOCAL_EXPECTED UINT64_C(0x10203040506070a5)
 #define INDIRECT_LOCAL_EXPECTED UINT64_C(0x8877665544332211)
-#define INLINE_LOCAL_XOR UINT64_C(0x123456789abcdef0)
 #define INLINE_LOCAL_EXPECTED UINT64_C(0x02146638cadcae70)
 
 volatile uint64_t parameter_seed = UINT64_C(0x1122334455667788);
+volatile uint64_t inline_seed = INLINE_LOCAL_EXPECTED;
 uint64_t indirect_seed = INDIRECT_LOCAL_EXPECTED;
 
 __attribute__((noinline)) uint64_t inspect_parameter_value(uint64_t parameter) {
@@ -80,12 +80,12 @@ __attribute__((noinline)) uint64_t inspect_indirect_local(uint64_t* ptr) {
 
 static __attribute__((always_inline)) inline uint64_t inspect_inlined_local(
     uint64_t parameter) {
-  uint64_t inline_local = parameter ^ INLINE_LOCAL_XOR;
+  uint64_t inline_local = parameter;
   __asm__ volatile(".globl inlined_local_probe\n"
                    "inlined_local_probe:\n"
                    "nop\n"
+                   : "+D"(inline_local)
                    :
-                   : "m"(inline_local)
                    : "memory");
   return inline_local;
 }
@@ -101,5 +101,5 @@ int main(void) {
     return 4;
   }
   if (inspect_indirect_local(&indirect_seed) != INDIRECT_LOCAL_EXPECTED) return 5;
-  return inspect_inlined_local(parameter) == INLINE_LOCAL_EXPECTED ? 0 : 6;
+  return inspect_inlined_local(inline_seed) == INLINE_LOCAL_EXPECTED ? 0 : 6;
 }

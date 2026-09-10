60af1ee207f4f73eafbffa4100e52643e370ad7e
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-10 13:17:05 +0800

    test: add cross-file inline compiler fixture

---
+++ tests/fixtures/inline_core_fixture.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+
+static __attribute__((always_inline)) inline int inline_inner(int seed) {
+  int inner_only = seed + 7;
+  int shadow_value = inner_only ^ 0x55;
+  __asm__ volatile("" : "+r"(inner_only), "+r"(shadow_value) :: "memory");
+  __asm__ volatile(
+      ".globl snapshot_inline_crash_probe\n"
+      "snapshot_inline_crash_probe:\n"
+      "movl %0, (%%rax)\n"
+      :
+      : "r"(inner_only + shadow_value), "a"((uintptr_t)0)
+      : "memory");
+  __builtin_unreachable();
+}
+
+static __attribute__((always_inline)) inline int inline_outer(int seed) {
+  int outer_only = seed * 3;
+  int shadow_value = outer_only + 11;
+  __asm__ volatile("" : "+r"(outer_only), "+r"(shadow_value) :: "memory");
+  return inline_inner(outer_only + shadow_value);
+}

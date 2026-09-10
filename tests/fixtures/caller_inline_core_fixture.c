87c08fb556b937e6a57fe7b840f212c27edbaefa
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-10 19:26:29 +0800

    test: provide selected-inline aggregate storage

--- tests/fixtures/caller_inline_core_fixture.c
+++ tests/fixtures/caller_inline_core_fixture.c
@@ -1,6 +1,9 @@
 #include "caller_inline_core_fixture.h"
 
 int caller_inline_pointee = 0x02468ace;
+int caller_inline_member_pointee = 0x13579bdf;
+struct CallerInlineAggregate caller_inline_aggregate = {
+    0x11223344, &caller_inline_member_pointee};
 
 __attribute__((noinline)) static int caller_physical_frame(int seed) {
   int physical_guard = seed;

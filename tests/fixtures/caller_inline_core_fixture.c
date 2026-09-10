42d58fe55c6a962345f4159485b37a3cbabe8ed5
LeiZ <52287354+Lei-TzuY@users.noreply.github.com>
2026-09-10 16:00:23 +0800

    test: make caller inline scalar runtime-derived

--- tests/fixtures/caller_inline_core_fixture.c
+++ tests/fixtures/caller_inline_core_fixture.c
@@ -1,8 +1,11 @@
 #include "caller_inline_core_fixture.h"
 
-__attribute__((noinline)) static int caller_physical_frame(void) {
-  int physical_guard = 21;
+__attribute__((noinline)) static int caller_physical_frame(int seed) {
+  int physical_guard = seed;
   return caller_inline_outer(physical_guard);
 }
 
-int main(void) { return caller_physical_frame(); }
+int main(int argc, char** argv) {
+  (void)argv;
+  return caller_physical_frame(argc + 20);
+}
